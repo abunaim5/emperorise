9b502824ff79ea12434625b9d887503290bf6700
abunaim5 <abunaim140@gmail.com>
2024-04-09 04:54:22 +0600

    added estate card component and load estate data and display the data

--- src/components/Estate/Estate.jsx
+++ src/components/Estate/Estate.jsx
@@ -1,22 +1,18 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom"
+import EstateCard from "../EstateCard/EstateCard";
 
 const Estate = () => {
+    const estates = useLoaderData()
+    console.log(estates);
+
     return (
         <div>
             <h3 className="text-3xl font-bold text-[#333333]">Estates</h3>
-            <div>
-                <div className="card w-96 glass rounded-none mt-10 text-[#333333]">
-                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="car!" /></figure>
-                    <div className="card-body p-6">
-                        <h2 className="card-title">Life hack</h2>
-                        <p>Status: </p>
-                        <p>Segment: </p>
-                        <p>Location: </p>
-                        <div className="card-actions">
-                        <Link to='/details'><button className="btn w-auto h-auto min-h-max rounded-none px-7 py-2 font-semibold hover:text-[#333333] border border-[#C0C0C0] hover:border-transparent text-[#333333] bg-transparent hover:bg-[#C0C0C0]">View Property</button></Link>
-                        </div>
-                    </div>
-                </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+                {
+                    estates.map(estate => <EstateCard key={estate.id} estate={estate}></EstateCard>)
+                }
             </div>
         </div>
     );
--- src/components/EstateCard/EstateCard.jsx
+++ src/components/EstateCard/EstateCard.jsx
@@ -1,10 +1,29 @@
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
 
-const EstateCard = () => {
+const EstateCard = ({estate}) => {
+    const {image, estate_title, status, segment_name, location} = estate;
+
     return (
         <div>
-            
+            <div className="card glass rounded-none text-[#333333]">
+                <figure><img className="h-[245px] w-full" src={image} alt="car!" /></figure>
+                <div className="card-body p-6">
+                    <h2 className="card-title">{estate_title}</h2>
+                    <p>Status: {status}</p>
+                    <p>Segment: {segment_name}</p>
+                    <p>Location: {location}</p>
+                    <div className="card-actions mt-4">
+                        <Link to='/details'><button className="btn w-auto h-auto min-h-max rounded-none px-7 py-2 font-semibold hover:text-[#333333] border border-[#C0C0C0] hover:border-transparent text-[#333333] bg-transparent hover:bg-[#C0C0C0]">View Property</button></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
+
+EstateCard.propTypes = {
+    estate: PropTypes.object.isRequired
+}
 
 export default EstateCard;
